ddde06cddd98770f5d09056fc3717e31f162b436
Devin Austin <devin.austin@gmail.com>
2019-06-06 15:46:29 -0600

    switched around discord bot api version

--- src/main/java/io/dja/arnold/Bot.java
+++ src/main/java/io/dja/arnold/Bot.java
@@ -11,24 +11,24 @@ public class Bot {
 
   public static void main(String[] args) {
     DiscordClientBuilder builder = new DiscordClientBuilder(args[0]);
-    DiscordClient client = builder.build();
-
-    client.getEventDispatcher().on(ReadyEvent.class)
-        .subscribe(event -> {
-          User self = event.getSelf();
-          System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
-        });
-
-    client.getEventDispatcher().on(MessageCreateEvent.class)
-        .map(MessageCreateEvent::getMessage)
-        .filterWhen(message -> message.getAuthor().map(user -> !user.isBot()))
-        .filter(message -> message.getContent().orElse("").equalsIgnoreCase("!ping"))
-        .flatMap(Message::getChannel)
-        .flatMap(channel -> channel.createMessage("Pong!"))
-        .subscribe();
-
-
-    client.login().block();
+      DiscordClient client = builder.build();
+    
+      client.getEventDispatcher().on(ReadyEvent.class)
+              .subscribe(event -> {
+                  User self = event.getSelf();
+                  System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+              });
+    
+      client.getEventDispatcher().on(MessageCreateEvent.class)
+              .map(MessageCreateEvent::getMessage)
+              .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+              .filter(message -> message.getContent().orElse("").equalsIgnoreCase("!ping"))
+              .flatMap(Message::getChannel)
+              .flatMap(channel -> channel.createMessage("Pong!"))
+              .subscribe();
+    
+    
+      client.login().block();
   }
 
 }
