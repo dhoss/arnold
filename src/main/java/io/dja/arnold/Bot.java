7adfaad58dec97e578b6f630632c81c35323f0b9
Devin Austin <devin.austin@gmail.com>
2019-06-07 14:59:23 -0600

    organized Ping command accordingly, this closes #8

--- src/main/java/io/dja/arnold/Bot.java
+++ src/main/java/io/dja/arnold/Bot.java
@@ -1,5 +1,6 @@
 package io.dja.arnold;
 
+import io.dja.arnold.heartbeat.Ping;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
@@ -12,11 +13,7 @@ public class Bot {
       DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
     
       // Add a listener which answers with "Pong!" if someone writes "!ping"
-      api.addMessageCreateListener(event -> {
-          if (event.getMessageContent().equalsIgnoreCase("!ping")) {
-              event.getChannel().sendMessage("Pong!");
-          }
-      });
+      api.addMessageCreateListener(new Ping());
     
       // Print the invite url of your bot
       System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
